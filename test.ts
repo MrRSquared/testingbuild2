9004835745401fb22c20b95ff402197b13d838a4
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2021-02-01 22:36:25 -0500

    FIxed broken build :)

--- test.ts
+++ test.ts
@@ -1,2 +1,2 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-basic.showNumber(custom.fib("hi"))
+basic.showNumber(custom.fib((4))) //This would break if we used a string instead of a number. That is how the Unit Tests work.
